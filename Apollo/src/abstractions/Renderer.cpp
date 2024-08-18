686ada160e3068af2f858dbbd10cf8e223f5645c
luizhverrisouza <luiz.verri.souza@gmail.com>
2024-08-17 22:19:40 -0300

    Segundo grupo de arquivos

--- Apollo/src/abstractions/Renderer.cpp
+++
@@ -1,9 +0,0 @@
-#include "Renderer.h"
-
-void Renderer::Draw(VertexArrayID& vaoID, IndexBufferID_Count& iboID_Count, Shader& shader) const
-{
-	shader.Bind();
-    VertexArrayManager::Bind(vaoID);
-	IndexBufferManager::Bind(iboID_Count.ID);
-	glDrawElements(GL_TRIANGLES, iboID_Count.count, GL_UNSIGNED_INT, nullptr);
-}
